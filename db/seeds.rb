578fede78c7d0aa3522cc09cc2fa35dd0c651708
Christopher Wright <cmwright33@gmail.com>
2013-09-01 15:01:04 -0400

    added some seed info for idea and routed the ideas through the index.html idea page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+100.times do |n|
+  Idea.create(title: "#{n}", content: "this is great content for #{n} idea", tag: "this #{n}", user_id: "#{n}")
+end
+
+
+
