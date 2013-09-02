6039862c2e02899d5bf616c91b4d9cb2fa0d1b3a
Christopher Wright <cmwright33@gmail.com>
2013-09-02 17:45:30 -0400

    js works but new page still isn't appending

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,22 +1,33 @@
 
 $(function() {
-  return $(window).scroll(function() {
-    if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-      return alert('near bottom');
-    }
-  });
+  // return $(window).scroll(function() {
+  //   if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+  //     return alert('near bottom');
+  //   }
+
+  // });
 
 
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url;
+      url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more Ideas...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
 
-
-  $.ajax({
-    url: '/ideas',
-    dataType: 'json',
-    type: 'get'
-  }).done(function(data){
-    console.log(data);
-    console.log(data.id);
-});
+//   $.ajax({
+//     url: '/ideas',
+//     dataType: 'json',
+//     type: 'get'
+//   }).done(function(data){
+//     console.log(data);
+//     console.log(data.id);
+// });
 
 
 
