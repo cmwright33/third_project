db500c3734f59e1355dc9333348a72f8f2b7f37e
Tim <tim.blonski@gmail.com>
2013-09-02 21:08:12 -0400

    ajax calls working for comments and new ideas

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,22 +4,27 @@
 $(function () {
 
   $('#submitButton').on('click',function(){
-  var idea = {"title" : $('#title').val(),
-              "content" : $('#content').val(),
-              "tag" : $('#tag').val()
-              };
-  $.ajax({
-    url: '/save/idea',
-    dataType: 'json',
-    type: 'post',
-    data: idea
-  }).done(function(data){
-    console.log(data);
-  $('#' + data.user_id).append($('<div id=" '+ data.id +' "></div>').append($('<li>'+ data.title +'</li>')));
+    var idea = {
+      "title" : $('#title').val(),
+      "content" : $('#content').val(),
+      "tag" : $('#tag').val()
+      };
+    $.ajax({
+      url: '/save/idea',
+      dataType: 'json',
+      type: 'post',
+      data: idea
+    }).done(function(data){
+      console.log(data);
+      var div = '<div id=" '+ data.id +' "></div>';
+      var li = '<li>'+ data.title +'</li>';
+      var input = '<input id="comment" type="text" name="comment">';
+      var button = '<button id=" '+ data.id * 2 +' " class="commentButton">submit</button>';
+      $('#' + data.user_id).prepend(div).append(li).append(input).append(button);
     });
-  $('#title').val('');
-  $('#content').val('');
-  $('#tag').val('');
+    $('#title').val('');
+    $('#content').val('');
+    $('#tag').val('');
   });
 
 
@@ -27,7 +32,7 @@ $(function () {
     ideaId = this.id / 2;
     text = $('#comment').val();
     console.log(text);
-    var comment = {"content" : $('#comment').val(),
+    var comment = {"content" : text,
                    "ideaId" : ideaId
                   };
     console.log(comment);
@@ -38,6 +43,8 @@ $(function () {
     data: comment
     }).done(function(data){
     console.log(data);
+    var li = '<li>' + text + '</li>';
+    $('#'+ ideaId +' ul').prepend(li);
     });
   });
 
