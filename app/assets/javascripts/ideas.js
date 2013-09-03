8498ed92f754bfeeb08adc51fa4c86652cb0ad35
Christopher Wright <cmwright33@gmail.com>
2013-09-03 13:05:18 -0400

    cleaned up javascript files

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,12 +1,5 @@
 
 $(function() {
-  // return $(window).scroll(function() {
-  //   if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-  //     return alert('near bottom');
-  //   }
-
-  // });
-
 
   if ($('.pagination').length) {
     $(window).scroll(function() {
@@ -16,29 +9,10 @@ $(function() {
         $('.pagination').text("Fetching more Ideas...");
         return $.getScript(url);
         console.log(url);
-        //   $.ajax({
-        //   url: 'url',
-        //   dataType: 'script',
-        //   type: 'get'
-        // }).done(function(){
-        //   console.log(script);
-        // });
-
       }
     });
     return $(window).scroll();
   }
-
-
-
-
-      // $('.pagination a').on("click", function () {
-      //   $('.pagination').html('Page is loading...');
-      //   $.get(this.href, null, null, 'script');
-      //   return false;
-      // });
-
-
 
 
 });
