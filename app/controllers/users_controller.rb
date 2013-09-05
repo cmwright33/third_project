ec0d0a4d2a336653d904994adffd5f748c046e68
Christopher Wright <cmwright33@gmail.com>
2013-09-05 03:57:48 -0400

    think i sorted out alot of the merging problems, i think

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
 
   def index
-      @user = User.first
-      @user = current_user
-      @user_ideas = @user.ideas.order("created_at DESC")
-      @user_ideas.each do |idea|
-        idea.comments.order("created_at DESC")
-      end
-      @user_vote_ids = [];
-      @user.votes.each do |vote|
-      @user_vote_ids << vote.idea_id
+      if current_user.nil?
+        redirect_to new_user_session_path
+      else
+        @user = current_user
+        @user_ideas = @user.ideas.order("created_at DESC")
+        @user_ideas.each do |idea|
+          idea.comments.order("created_at DESC")
+        end
+        @user_vote_ids = [];
+        @user.votes.each do |vote|
+        @user_vote_ids << vote.idea_id
+        end
       end
   end
 
