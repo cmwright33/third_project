33b16d7dfa580ae1d9cc3bd8ff9f533c278068d3
Tim <tim.blonski@gmail.com>
2013-09-05 17:20:40 -0400

    fixed devise log in. users can add username and bio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :bio, :github_URL, :portfolio_URL)}
+  end
 
 end
